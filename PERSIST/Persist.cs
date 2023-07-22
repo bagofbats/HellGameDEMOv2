85275919eed2c7576cdf696b0202eb451853790c
bagofbats <colincrowe0@gmail.com>
2023-07-22 00:22:29 -0400

    more tiles

--- PERSIST/Persist.cs
+++ PERSIST/Persist.cs
@@ -10,7 +10,7 @@ namespace PERSIST
 {
     public class Persist : Game
     {
-        private bool debug = false;
+        private bool debug = true;
 
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
